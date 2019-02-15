405d4c3bab4bd0caf628b17f32bf7f57e74d9fab
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-14 23:16:14 -0700

    style

--- magic_8_ball.rb
+++ magic_8_ball.rb
@@ -2,29 +2,30 @@ require 'pry'
 require 'colorize'
 
 class Magic8
+  # String.color_samples
 
   def initialize
     @answers = [
-      "It is certain.",
-      "It is decidedly so.",
-      "Without a doubt.",
-      "Yes - definitely.",
-      "You may rely on it.",
-      "As I see it, yes.",
-      "Most likely.",
-      "Outlook good.",
-      "Yes.",
-      "Signs point to yes.",
-      "Reply hazy, try again.",
-      "Ask again later.",
-      "Better not tell you now.",
-      "Cannot predict now.",
-      "Concentrate and ask again.",
-      "Don't count on it.",
-      "My reply is no.",
-      "My sources say no.",
-      "Outlook not so good.",
-      "Very doubtful."
+      "It is certain",
+      "It is decidedly so",
+      "Without a doubt",
+      "Yes - definitely",
+      "You may rely on it",
+      "As I see it, yes",
+      "Most likely",
+      "Outlook good",
+      "Yes",
+      "Signs point to yes",
+      "Reply hazy, try again",
+      "Ask again later",
+      "Better not tell you now",
+      "Cannot predict now",
+      "Concentrate and ask again",
+      "Don't count on it",
+      "My reply is no",
+      "My sources say no",
+      "Outlook not so good",
+      "Very doubtful"
     ]
 
     @answers_backup = @answers.map(&:clone)
@@ -33,22 +34,22 @@ class Magic8
   def greeting
     puts ""
     puts ""
-    puts '    Welcome to Magic 8 Ball'.colorize(:yellow)
-    puts "--------------------------------".colorize(:yellow)
-    puts "88888888888888888888888888888888".colorize(:green)
-    puts "--------------------------------".colorize(:yellow)
+    puts '    Welcome to Magic 8 Ball'.colorize(:magenta)
+    puts "--------------------------------".colorize(:light_red)
+    puts "88888888888888888888888888888888".colorize(:cyan)
+    puts "--------------------------------".colorize(:light_red)
     menu
   end
 
   def menu
     puts "-------------------------".colorize(:yellow)
-    puts "What would you wish to do?".colorize(:green)
+    puts "What would you wish to do?".colorize(:magenta)
     puts "-------------------------".colorize(:yellow)
-    puts "1) Ask A Question"
-    puts "2) Add An Answer"
-    puts "3) Answer Reset"
-    puts "4) Print All Answers"
-    puts "5) Exit"
+    puts "1) Ask A Question".colorize(:light_red)
+    puts "2) Add An Answer".colorize(:light_red)
+    puts "3) Answer Reset".colorize(:light_red)
+    puts "4) Print All Answers".colorize(:light_red)
+    puts "5) Exit".colorize(:red)
     @user_input = gets.to_i
     menu_choice
   end
@@ -64,7 +65,7 @@ class Magic8
     when 4
       show_answers
     when 5
-      puts "Goodbye"
+      puts "Goodbye".colorize(:magenta)
       sleep(2)
       puts `clear`
       exit
@@ -75,38 +76,39 @@ class Magic8
   end
 
   def question
-    binding.pry
-    puts "Ask Me A Question"
+    puts "Ask Me A Question".colorize(:magenta)
     question = gets.strip
-    puts '.......shaking'
-    puts @answers.sample
+    puts '.......shaking'.colorize(:cyan)
+    puts '~~~~~~~~~~~~~~~~~~~'.colorize(:light_red)
+    puts @answers.sample.colorize(:light_blue)
+    puts '~~~~~~~~~~~~~~~~~~~'.colorize(:light_red)
     menu
   end
 
   def add_answer
-    puts "What Answer Do You Wish To Add?"
+    puts "What Answer Do You Wish To Add?".colorize(:magenta)
     user_answer = gets.strip
     if @answers.include? user_answer
-      puts "Answer Already Exists"
+      puts "Answer Already Exists".colorize(:red)
       add_answer
     else
       @answers.push user_answer
-      puts "Your Answers Been Added"
+      puts "Your Answers Been Added".colorize(:green)
       menu
     end
   end
 
   def answer_reset
-    puts "Are You Sure? y/n"
+    puts "Are You Sure? y/n".colorize(:red)
     input = gets.strip.downcase
     if input == 'y'
       @answers = @answers_backup.map(&:clone)
       puts `clear`
-      puts "The 8 Ball Has Been Reset"
+      puts "The 8 Ball Has Been Reset".colorize(:magenta)
       sleep(2)
       menu
     elsif input == 'n'
-      puts 'Taking You Back To Main Menu'
+      puts 'Taking You Back To Main Menu'.colorize(:magenta)
       sleep(2)
       menu
     else
@@ -117,7 +119,7 @@ class Magic8
   end
 
   def show_answers
-    puts @answers
+    puts @answers.colorize(:magenta)
     menu
   end
 
